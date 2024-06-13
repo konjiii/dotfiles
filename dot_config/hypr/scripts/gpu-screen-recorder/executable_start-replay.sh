80fdbcd0b6bea068348644627906750ecc20d0d4
konjiii <kenji.opdam@gmail.com>
2024-06-13 16:09:25 +0200

    added pass for discord mute and improved other keybinds

--- dot_config/hypr/scripts/gpu-screen-recorder/executable_start-replay.sh
+++ dot_config/hypr/scripts/gpu-screen-recorder/executable_start-replay.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# wait for 5 seconds for the window manager to start
+sleep 5
+
 video_path="$HOME/media/Videos/clips"
 mkdir -p "$video_path"
 gpu-screen-recorder -w DP-1 -f 60 -a "$(pactl get-default-sink).monitor" -c mp4 -r 300 -o "$video_path"
