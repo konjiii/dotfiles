a5bf9d1a37d8351f8a8644efa91b8ebb54b67d3e
konjiii <kenji.opdam@gmail.com>
2024-10-24 23:54:06 +0200

    convert chezmoi -> gnu stow

--- dot_config/scripts/executable_mouse_accel_off.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-deviceid=$(xinput | grep ⎜ | grep Logitech | grep "Receiver  " | awk '{print $6}' | sed 's/id=//g')
-
-xinput --set-prop $deviceid "libinput Accel Profile Enabled" 0 1 0
