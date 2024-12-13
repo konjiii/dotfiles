e53cf5a48d95c518f580ff17cba7d76894494f23
konjiii <kenji.opdam@gmail.com>
2024-12-13 06:31:56 +0100

    changed volume notification script

--- hypr/.config/hypr/scripts/display_volume.sh
+++ hypr/.config/hypr/scripts/display_volume.sh
@@ -11,14 +11,22 @@ msgTag="volume"
 volume="$(pamixer --get-volume)"
 mute="$(pamixer --get-mute)"
 
+if [[ $volume -lt 34 ]]; then
+    icon="audio-volume-low"
+elif [[ $volume -lt 67 ]]; then
+    icon="audio-volume-medium"
+else
+    icon="audio-volume-high"
+fi
+
 if [[ $volume == 0 || "$mute" == true ]]; then
     # Show the sound muted notification
-    dunstify -a "changeVolume" -u low -i audio-volume-muted -h string:x-dunst-stack-tag:$msgTag \
-        "Volume: ${volume}%" "Volume muted"
+    dunstify -a "changeVolume" -u low -i /usr/share/icons/breeze-dark/status/24/audio-volume-muted.svg -h string:x-dunst-stack-tag:$msgTag \
+        -h int:value:$volume "Volume: $volume%" "Volume muted"
 else
     # Show the volume notification
-    dunstify -a "changeVolume" -u low -i audio-volume-high -h string:x-dunst-stack-tag:$msgTag \
-        -h int:value:"$volume" "Volume: ${volume}%"
+    dunstify -a "changeVolume" -u low -i /usr/share/icons/breeze-dark/status/24/$icon.svg -h string:x-dunst-stack-tag:$msgTag \
+        -h int:value:$volume "Volume: $volume%"
 fi
 
 # Play the volume changed sound
