c542b781d82f1f253344836649ef44c4ab8497ff
konjiii <kenji.opdam@gmail.com>
2024-12-13 06:48:50 +0100

    changed brightness notification script

--- hypr/.config/hypr/scripts/display_brightness.sh
+++ hypr/.config/hypr/scripts/display_brightness.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-# changeVolume
 
-# Arbitrary but unique message tag
-msgTag="volume"
+msgTag="volume/brightness"
 
 # Change the volume using alsa(might differ if you use pulseaudio)
 # amixer -c 0 set Master "$@" > /dev/null
@@ -11,8 +9,23 @@ msgTag="volume"
 max_brightness=$(brightnessctl m)
 brightness=$(brightnessctl g)
 brightness=$(($brightness * 100 / $max_brightness))
-echo $brightness
+
+if [[ $brightness -eq 0 ]]; then
+    icon="brightness-full"
+elif [[ $brightness -le 25 ]]; then
+    icon="brightness-low"
+elif [[ $brightness -le 50 ]]; then
+    icon="brightness-medium"
+elif [[ $brightness -le 75 ]]; then
+    icon="brightness-high"
+else
+    icon="brightness-full"
+fi
 
 # Show the volume notification
-dunstify -a "changeVolume" -u low -i audio-volume-high -h string:x-dunst-stack-tag:$msgTag \
--h int:value:"$brightness" "Brightness: ${brightness}%"
+dunstify \
+    -a "changeVolume/Brightness" \
+    -u low \
+    -i /usr/share/icons/Papirus/48x48/status/notification-display-$icon.svg \
+    -h string:x-dunst-stack-tag:$msgTag \
+    -h int:value:"$brightness" "Brightness: ${brightness}%"
