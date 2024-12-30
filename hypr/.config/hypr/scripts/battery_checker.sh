5bc1c02f778f0bdef8a11f60bedf3c2a846284c5
konjiii <kenji.opdam@gmail.com>
2024-12-30 01:36:17 +0100

    updated battery low message

--- hypr/.config/hypr/scripts/battery_checker.sh
+++ hypr/.config/hypr/scripts/battery_checker.sh
@@ -3,7 +3,7 @@ while true; do
     capacity=$(cat /sys/class/power_supply/BAT1/capacity)
     status=$(cat /sys/class/power_supply/BAT1/status)
     if [ "$capacity" -le 20 ] && [ "$status" != "Charging" ]; then
-        dunstify --urgency=CRITICAL "Battery Low" "Level: ${capacity}%"
+        dunstify --urgency=CRITICAL "❗ Battery Low"
         sleep 1200
     else
         sleep 120
