50fdf91faddb67043eb156984b71d5c89f451390
konjiii <kenji.opdam@gmail.com>
2025-01-19 18:33:14 +0100

    updated battery_checker script hyprland

--- hypr/.config/hypr/scripts/battery_checker.sh
+++ hypr/.config/hypr/scripts/battery_checker.sh
@@ -2,10 +2,17 @@
 while true; do
     capacity=$(cat /sys/class/power_supply/BAT1/capacity)
     status=$(cat /sys/class/power_supply/BAT1/status)
+    # show battery low notification
     if [ "$capacity" -le 20 ] && [ "$status" != "Charging" ]; then
-        dunstify --urgency=CRITICAL "❗ Battery Low"
-        sleep 1200
+        dunstify -r 1 --urgency=CRITICAL "❗ Battery Low"
+        sleep 120
+    # show battery high notification
+    elif [ "$capacity" -ge 80 ] && [ "$status" = "Charging" ]; then
+        dunstify -r 1 "🔋 Battery > 80%"
+        sleep 120
+    # close battery notification
     else
+        dunstctl close 1
         sleep 120
     fi
 done
