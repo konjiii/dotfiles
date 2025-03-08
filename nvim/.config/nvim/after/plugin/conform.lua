b5ba8b5ba3bccd4c5b712f9ed205db6a28fdddb2
konjiii <kenji.opdam@gmail.com>
2025-03-08 14:32:37 +0100

    changed from black to ruff

--- nvim/.config/nvim/after/plugin/conform.lua
+++ nvim/.config/nvim/after/plugin/conform.lua
@@ -2,7 +2,7 @@ require("conform").setup({
 	format_after_save = {},
 	formatters_by_ft = {
 		lua = { "stylua" },
-		python = { "black" },
+		python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
 		c = { "clang-format" },
 		cpp = { "clang-format" },
 		ocaml = { "ocamlformat" },
