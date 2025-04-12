ae56eca2e5059382d37a6f9a912449ae6fe378e9
konjiii <kenji.opdam@gmail.com>
2025-04-12 17:43:49 +0200

    added goose.nvim

--- nvim/.config/nvim/lua/plugins/plugins.lua
+++ nvim/.config/nvim/lua/plugins/plugins.lua
@@ -174,4 +174,20 @@ return {
 			require("distant"):setup()
 		end,
 	},
+	{
+		"azorng/goose.nvim",
+		branch = "main",
+		config = function()
+			require("goose").setup({})
+		end,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					anti_conceal = { enabled = false },
+				},
+			},
+		},
+	},
 }
