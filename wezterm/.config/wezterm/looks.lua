9cbf1b78b2f127ff2f4619e49273cd8534857657
konjiii <kenji.opdam@gmail.com>
2025-05-02 01:54:45 +0200

    remove kitty config

--- wezterm/.config/wezterm/looks.lua
+++ wezterm/.config/wezterm/looks.lua
@@ -1,10 +1,9 @@
-local wezterm = require("wezterm")
-
 local looks = {}
 
 function looks.apply_to_config(config)
 	config.color_scheme = "Catppuccin Mocha"
 	config.hide_tab_bar_if_only_one_tab = true
+	config.enable_scroll_bar = true
 end
 
 return looks
