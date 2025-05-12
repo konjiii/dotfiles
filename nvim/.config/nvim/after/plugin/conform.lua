0b409098d8e02a018239a67cfeb1d17b66a922fb
konjiii <kenji.opdam@gmail.com>
2025-05-12 18:01:53 +0200

    remove ruff_fix

--- nvim/.config/nvim/after/plugin/conform.lua
+++ nvim/.config/nvim/after/plugin/conform.lua
@@ -10,10 +10,9 @@ conform.setup({
     format_after_save = {},
     formatters_by_ft = {
         lua = { "stylua" },
-        python = { "ruff_fix", "ruff_format" },
+        python = { "ruff_format", "ruff_organize_imports" },
         c = { "clang-format" },
         cpp = { "clang-format" },
-        ocaml = { "ocamlformat" },
         sh = { "shfmt" },
         svelte = { "prettier" },
         html = { "prettier" },
