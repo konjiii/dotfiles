d476ad653067fc0e23a3080e9e8916a24a75fb09
konjiii <kenji.opdam@gmail.com>
2025-05-17 17:59:51 +0200

    change sessionizer list

--- wezterm/.config/wezterm/sessionizer.lua
+++ wezterm/.config/wezterm/sessionizer.lua
@@ -1,10 +1,15 @@
+local wezterm = require("wezterm")
 local sessionizer = {}
 
 sessionizer.schema = {
-	{ label = "dotfiles", id = "./dotfiles" },
-	Sessionizer.DefaultWorkspace({}),
-	Sessionizer.AllActiveWorkspaces({}),
-	Sessionizer.FdSearch("./mhome"), -- Searches for git repos in ~/my_projects
+    { label = "dotfiles", id = "./dotfiles" },
+    Sessionizer.DefaultWorkspace({}),
+    Sessionizer.AllActiveWorkspaces({}),
+    Sessionizer.FdSearch(wezterm.home_dir .. "/mhome"),
+
+    processing = Sessionizer.for_each_entry(function(entry)
+        entry.label = entry.label:gsub(wezterm.home_dir .. "/", "")
+    end),
 }
 
 return sessionizer
