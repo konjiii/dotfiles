de3c14977c4e2590c0080e9d660f6c54d1b064c7
konjiii <kenji.opdam@gmail.com>
2025-05-24 01:57:22 +0200

    texlab config

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -31,6 +31,28 @@ require("mason-lspconfig").setup({
             require("lspconfig").lua_ls.setup(lua_opts)
         end,
     },
+    ensure_installed = {
+        "zls",
+        "lua_ls",
+        "clangd",
+        "pyright",
+        "bashls",
+        "gopls",
+        "rust_analyzer",
+        -- "texlab",
+        "asm_lsp",
+        "svelte",
+        "ts_ls",
+        -- only lsps can be ensure installed
+        -- "stylua",
+        -- "cpptools",
+        -- "debugpy",
+        -- "shfmt",
+        -- "texfmt",
+        -- "bibtex-tidy",
+        -- "asmfmt",
+        -- "prettier",
+    },
 })
 
 -- setup nvim-cmp
