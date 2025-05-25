a93d6034d2f4718b13e56007e001aa7f1c662eb5
konjiii <kenji.opdam@gmail.com>
2025-05-25 21:36:58 +0200

    remove thefuck from zshrc

--- nvim/.config/nvim/lua/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/plugins/vimtex.lua
@@ -3,7 +3,9 @@ return {
     lazy = false, -- we don't want to lazy load VimTeX
     -- tag = "v2.15", -- uncomment to pin to a specific release
     init = function()
-        vim.g.vimtex_view_method = "zathura"
+        -- vim.g.vimtex_view_method = "zathura"
+        vim.g.vimtex_view_general_viewer = "okular"
+        vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
 
         vim.opt.conceallevel = 2
         vim.g.vimtex_syntax_conceal = {
