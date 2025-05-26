2f8c11bf67f3b7056b37a5d31dd86784ea1a9c93
konjiii <kenji.opdam@gmail.com>
2025-05-26 18:53:09 +0200

    tex and markdown specific keymap in autocmd

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,11 +1,1 @@
 vim.opt_local.wrap = true
-
--- j->gj and k->gk to navigate wrapped lines
-vim.keymap.set({ "n", "v" }, "j", "gj")
-vim.keymap.set({ "n", "v" }, "k", "gk")
-
--- skip to end or beginning of line in normal and visual mode
-vim.keymap.set("n", "L", "g$")
-vim.keymap.set("n", "H", "g^")
-vim.keymap.set("v", "L", "g$")
-vim.keymap.set("v", "H", "g^")
