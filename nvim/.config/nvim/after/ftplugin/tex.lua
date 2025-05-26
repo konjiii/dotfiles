3dce52ce25b426594b99264843274ef587c1bc85
konjiii <kenji.opdam@gmail.com>
2025-05-26 16:53:54 +0200

    end of line of current wrapped line

--- nvim/.config/nvim/after/ftplugin/tex.lua
+++ nvim/.config/nvim/after/ftplugin/tex.lua
@@ -4,3 +4,8 @@ vim.opt_local.wrap = true
 vim.keymap.set({ "n", "v" }, "j", "gj")
 vim.keymap.set({ "n", "v" }, "k", "gk")
 
+-- skip to end or beginning of line in normal and visual mode
+vim.keymap.set("n", "L", "g$")
+vim.keymap.set("n", "H", "g^")
+vim.keymap.set("v", "L", "g$")
+vim.keymap.set("v", "H", "g^")
