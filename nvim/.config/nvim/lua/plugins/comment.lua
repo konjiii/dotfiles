1c7e36104fafbda11a488e962133821251097be0
konjiii <kenji.opdam@gmail.com>
2025-11-07 03:08:45 +0100

    nvim: add keybind for block comments

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,2 +1,2 @@
 -- ctrl+/ to comment out block of code
-return { "numToStr/Comment.nvim", opts = { opleader = { line = "<C-_>" } } }
+return { "numToStr/Comment.nvim", opts = { opleader = { line = "<C-_>", block = "?" } } }
