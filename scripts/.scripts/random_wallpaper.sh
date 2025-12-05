7112f5adc26eb936f1cbaeb06b9397ccc353c96d
konjiii <kenji.opdam@gmail.com>
2025-12-05 05:12:50 +0100

    change wallpaper and script location

--- scripts/.scripts/random_wallpaper.sh
+++ scripts/.scripts/random_wallpaper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-WALLPAPER_DIR=~/.config/hypr/wallpapers
+WALLPAPER_DIR=~/.wallpapers
 WALLPAPER="$(fd . $WALLPAPER_DIR | shuf -n 1)"
 
 cp "$WALLPAPER" "$WALLPAPER_DIR/current.png"
